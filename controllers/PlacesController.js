b033fb83c2b4d3c147d963d9d7578cf5071b1bd3
Alfredo Poveda <alfpovsistemas@gmail.com>
2020-03-26 00:38:52 -0400

    Controladores

---
+++ controllers/PlacesController.js
@@ -0,0 +1,83 @@
+const Places = require("../models/Places");
+
+class PlacesController {
+  static async index(req, res) {
+    try {
+      const docs = await Places.find({});
+
+      res.json(docs);
+    } catch (error) {
+      console.error(error);
+      res.json(error);
+    }
+  }
+
+  static async create(req, res) {
+    try {
+      const doc = await Places.create({
+        title: req.body.title,
+        description: req.body.description,
+        acceptsCreditCard: req.body.acceptsCreditCard,
+        openHours: req.body.openHours,
+        closeHour: req.body.closeHour
+      });
+
+      res.json(doc);
+    } catch (error) {
+      console.error(error);
+      res.json(error);
+    }
+  }
+
+  static async show(req, res) {
+    try {
+      const doc = await Places.findById(req.params.id);
+
+      res.json(doc);
+    } catch (error) {
+      console.error(error);
+      res.json(error);
+    }
+  }
+
+  static async update(req, res) {
+    try {
+      const attributes = [
+        "title",
+        "description",
+        "acceptsCreditCard",
+        "openHours",
+        "closeHour"
+      ];
+      const place = {};
+
+      attributes.map(attr => {
+        if (req.body.hasOwnProperty(attr)) {
+          place[attr] = req.body[attr];
+        }
+      });
+
+      const doc = await Places.findOneAndUpdate({ _id: req.params.id }, place, {
+        new: true
+      });
+
+      res.json(doc);
+    } catch (error) {
+      console.error(error);
+      res.json(error);
+    }
+  }
+
+  static async destroy(req, res) {
+    try {
+      const doc = await Places.findByIdAndRemove(req.params.id);
+
+      res.json(doc);
+    } catch (error) {
+      console.error(error);
+      res.json(error);
+    }
+  }
+}
+
+module.exports = PlacesController;
--- routes/places.js
+++ routes/places.js
@@ -2,85 +2,17 @@ const express = require("express");
 
 const router = express.Router();
 
-const Places = require("../models/places");
+const Places = require("../controllers/PlacesController");
 
 router
   .route("/")
-  .get(async (req, res) => {
-    try {
-      const docs = await Places.find({});
-
-      res.json(docs);
-    } catch (error) {
-      console.error(error);
-      res.json(error);
-    }
-  })
-  .post(async (req, res) => {
-    try {
-      const doc = await Places.create({
-        title: req.body.title,
-        description: req.body.description,
-        acceptsCreditCard: req.body.acceptsCreditCard,
-        openHours: req.body.openHours,
-        closeHour: req.body.closeHour
-      });
-
-      res.json(doc);
-    } catch (error) {
-      console.error(error);
-      res.json(error);
-    }
-  });
+  .get(Places.index)
+  .post(Places.create);
 
 router
   .route("/:id")
-  .get(async (req, res) => {
-    try {
-      const doc = await Places.findById(req.params.id);
-
-      res.json(doc);
-    } catch (error) {
-      console.error(error);
-      res.json(error);
-    }
-  })
-  .put(async (req, res) => {
-    try {
-      const attributes = [
-        "title",
-        "description",
-        "acceptsCreditCard",
-        "openHours",
-        "closeHour"
-      ];
-      const place = {};
-
-      attributes.map(attr => {
-        if (req.body.hasOwnProperty(attr)) {
-          place[attr] = req.body[attr];
-        }
-      });
-
-      const doc = await Places.findOneAndUpdate({ _id: req.params.id }, place, {
-        new: true
-      });
-
-      res.json(doc);
-    } catch (error) {
-      console.error(error);
-      res.json(error);
-    }
-  })
-  .delete(async (req, res) => {
-    try {
-      const doc = await Places.findByIdAndRemove(req.params.id);
-
-      res.json(doc);
-    } catch (error) {
-      console.error(error);
-      res.json(error);
-    }
-  });
+  .get(Places.show)
+  .put(Places.update)
+  .delete(Places.destroy);
 
 module.exports = router;
